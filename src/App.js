6cf0b545cecbf2c0bf00ce3e64b4915d6e32d1b4
Felippe <felipperian365@gmail.com>
2022-08-21 14:43:20 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Made in <code>ReactJS</code>
         </p>
+        <p>by Felippe</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello World 🐱
         </a>
       </header>
     </div>
