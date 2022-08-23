9fc09be185af9e6f28698af8ca74f1cc753a1c61
Felippe <felipperian365@gmail.com>
2022-08-22 23:17:17 -0300

    boa noite

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Made in <code>ReactJS</code>
+          Boa noite BATMAN <code>ReactJS</code>
         </p>
         <p>by Felippe</p>
         <a
